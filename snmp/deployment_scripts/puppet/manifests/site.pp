ccba37ca64e080592fc36bde7862ee6ed6ecd9ee
root <root@fuel.claflinclan.local>
2015-02-20 07:00:10 +0000

    remove deploy, add puppet info

---
+++ snmp/deployment_scripts/puppet/manifests/site.pp
@@ -0,0 +1,11 @@
+$fuel_settings = parseyaml(file('/etc/astute.yaml'))
+
+# Just apply snmp settings
+class { 'snmp':
+  agentaddress => 'udp:$fuel_settings['snmp']['snmp_polling_port']',
+  rocommunity  => $fuel_settings['snmp']['snmp_read_community'],
+  ro_network   => $fuel_settings['snmp']['snmp_monitor_host'],
+  contact      => $fuel_settings['snmp']['snmp_contact'],
+  location     => $fuel_settings['snmp']['snmp_location'],
+}
+
