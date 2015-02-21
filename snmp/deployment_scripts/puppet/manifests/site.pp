1ae49608fdbe41834db30aa6aa44a602b166080d
root <root@fuel.claflinclan.local>
2015-02-21 05:49:56 +0000

    change post to pre

--- snmp/deployment_scripts/puppet/manifests/site.pp
+++ snmp/deployment_scripts/puppet/manifests/site.pp
@@ -2,7 +2,7 @@ $fuel_settings = parseyaml(file('/etc/astute.yaml'))
 
 # Just apply snmp settings
 class { 'snmp':
-  agentaddress => 'udp:$fuel_settings['snmp']['snmp_polling_port']',
+  agentaddress => $fuel_settings['snmp']['snmp_polling_port'],
   rocommunity  => $fuel_settings['snmp']['snmp_read_community'],
   ro_network   => $fuel_settings['snmp']['snmp_monitor_host'],
   contact      => $fuel_settings['snmp']['snmp_contact'],
