62cb8f89dfeb65d04905ba5550cc8f2ed65042be
root <github@claflinclan.com>
2015-03-05 18:44:13 +0000

    removing unneeded {}

--- snmp/deployment_scripts/puppet/manifests/site.pp
+++ snmp/deployment_scripts/puppet/manifests/site.pp
@@ -8,9 +8,9 @@ notice('PLUGIN: snmp')
 
 class { 'snmp':
   agentaddress  => [ "udp:${snmp_polling_port}", "udp6:${snmp_polling_port}" ],
-  ro_community  => ${snmp_read_community},
-  ro_community6 => ${snmp_read_community},
-  ro_network    => ${snmp_monitor_host},
+  ro_community  => $snmp_read_community,
+  ro_community6 => $snmp_read_community,
+  ro_network    => $snmp_monitor_host,
   com2sec       => [ "notConfigUser ${snmp_monitor_host} ${snmp_read_community}" ],
   com2sec6      => [ "notConfigUser ${snmp_monitor_host} ${snmp_read_community}" ],
   contact       => $fuel_settings['snmp']['snmp_contact'],
@@ -19,8 +19,8 @@ class { 'snmp':
 
 # Create a firewall rule
 firewall {"${snmp_polling_port} snmp ":
-  sport   => [ ${snmp_polling_port} ],
-  source  => ${snmp_monitor_host},
+  sport   => [ $snmp_polling_port ],
+  source  => $snmp_monitor_host,
   proto   => 'udp',
   action  => 'accept',
 }
